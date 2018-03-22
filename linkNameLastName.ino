dfbdd5ac8716f3cca7f2139d767cf0519d9da655
mehulwarade <mwarade@deakin.edu.au>
2018-03-22 14:10:42 +1100

    Create linkNameLastName.ino

---
+++ linkNameLastName.ino
@@ -0,0 +1,81 @@
+int led2 = D7;
+
+void setup() {
+  pinMode(led2, OUTPUT);
+
+}
+
+
+void loop() {
+//Showing "W"
+   digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+//Showing "A"
+digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+//Showing "R"
+digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+//Showing "A"
+digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+//Showing "D"
+digitalWrite(led2, HIGH);
+  delay(1000); //Long Blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+  
+  digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+//Showing "E"
+digitalWrite(led2, HIGH);
+  delay(500); //Short blink
+  digitalWrite(led2, LOW);
+  delay(1000);
+
+}
